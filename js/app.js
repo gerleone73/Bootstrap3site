1b5a76fe88f0ebf4711a440cc00c37afb1c629a6
Gerry Leonard <leonardger@gmail.com>
2015-03-30 13:16:28 +0100

    smoothscroll and glicthc fix on external js file

---
+++ js/app.js
@@ -0,0 +1,15 @@
+(function($) {
+
+ $('.navbar-collapse').click('li', function() {
+    if ($(window).width() < 770){
+  $('.navbar-collapse').collapse('hide');
+}
+
+});
+
+})(jQuery);
+
+
+  
+ 
+
