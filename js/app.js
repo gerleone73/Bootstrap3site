a11da04ea89fb35bc1f49eae0858b61abc2bf57d
Gerry Leonard <leonardger@gmail.com>
2015-03-30 17:03:15 +0100

    font awesome fail

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 (function($) {
 
+   setTimeout(function(){
+      $('#arrow').css('opacity', '0').addClass('slideDown');
+   }, 400);
+
  $('.navbar-collapse').click('li', function() {
     if ($(window).width() < 770){
   $('.navbar-collapse').collapse('hide');
