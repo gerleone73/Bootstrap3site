5f272013532d6a12b028abb24b79912c4ec819b7
Gerry Leonard <leonardger@gmail.com>
2015-03-31 12:10:00 +0100

    scroll top

--- js/app.js
+++ js/app.js
@@ -8,6 +8,26 @@
   }
 });
 
+  // create the back to top button
+$('footer').prepend('<a href="#" class="back-to-top">Back to Top</a>');
+
+var amountScrolled = 2600;
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > amountScrolled) {
+    $('a.back-to-top').fadeIn('slow');
+  } else {
+    $('a.back-to-top').fadeOut('slow');
+  }
+});
+
+$('a.back-to-top').click(function() {
+  $('body').animate({
+    scrollTop: 0
+  }, 'slow');
+  return false;
+});
+
    $('.navbar-collapse').collapse('hide'); // here to see if will be default collapse
 
    setTimeout(function(){
